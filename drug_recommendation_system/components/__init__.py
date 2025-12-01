12d5a31ed34e2624b50b2e95dc368bd6a8d50077
WI$DOM <141811303+CyberwizD@users.noreply.github.com>
2025-12-01 13:16:17 +0100

    feat: update component init file

---
+++ drug_recommendation_system/components/__init__.py
@@ -0,0 +1,13 @@
+"""Component exports"""
+from .navigation import navigation_bar
+from .drug_card import drug_card
+from .loading import loading_spinner, card_skeleton
+from .search_section import search_section
+
+__all__ = [
+    "navigation_bar",
+    "drug_card",
+    "loading_spinner",
+    "card_skeleton",
+    "search_section"
+]
