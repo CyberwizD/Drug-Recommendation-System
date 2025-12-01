cabf41d89a9cd62ad757579de1c313ad89f53f0b
WI$DOM <141811303+CyberwizD@users.noreply.github.com>
2025-12-01 13:20:01 +0100

    feat: update pages init file

---
+++ drug_recommendation_system/pages/__init__.py
@@ -0,0 +1,12 @@
+"""Page exports"""
+from .login import login_page
+from .home import home_page
+from .history import history_page
+from .analysis import analysis_page
+
+__all__ = [
+    "login_page",
+    "home_page",
+    "history_page",
+    "analysis_page"
+]
